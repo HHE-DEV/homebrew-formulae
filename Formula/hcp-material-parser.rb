8000e66db52dca905c35e2149c20833d92ceb7a1
Michael Housh <m-housh@users.noreply.github.com>
2022-02-10 15:07:03 +0000

    hcp-material-parser 0.1.9

--- Formula/hcp-material-parser.rb
+++ Formula/hcp-material-parser.rb
@@ -1,7 +1,7 @@
 class HcpMaterialParser < Formula
   desc "A command line utility to parse & update material pricing"
   homepage "https://github.com/HHE-DEV/hcp-material-parser"
-  url "https://github.com/HHE-DEV/hcp-material-parser.git", tag: "0.1.8", revision: "afab11cfbc142d4e22461842f17a48b18b809c76"
+  url "https://github.com/HHE-DEV/hcp-material-parser.git", tag: "0.1.9", revision: "576365b341be2a8ddb093e3ac00036cb2b14a92d"
   head "https://github.com/HHE-DEV/hcp-material-parser.git"
   license "MIT"
 
